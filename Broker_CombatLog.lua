ea6e98428b952fc8293a3cad6b28133d7a657617
Adirelle <adirelle@tagada-team.net>
2009-06-17 11:05:37 +0200

    Automatically starts to record when entering raid instance.

--- Broker_CombatLog.lua
+++ Broker_CombatLog.lua
@@ -1,18 +1,48 @@
 
+local forcedEnable = nil
+local forcedDisable = nil
+
 local dataobj = LibStub('LibDataBroker-1.1'):NewDataObject('Broker_CombatLog', {
 	type = 'data source',
 	icon = [[Interface\OptionsFrame\VoiceChat-Record]],
 	label = "CombatLog",
 	OnClick = function()
 		if LoggingCombat() then
+			if forcedEnable then
+				forcedEnable = nil
+				print('forcedEnable = nil')
+			else
+				forcedDisable = true
+				print('forcedDisable = true')
+			end
 			LoggingCombat(false)
 			print('Combat log recording stop.')
 		else
+			if forcedDisable then
+				forcedDisable = nil
+				print('forcedDisable = nil')
+			else
+				forcedEnable = true
+				print('forcedEnable = true')
+			end
 			LoggingCombat(true)
 			print('Combat log recording started.')
 		end
 	end,	
 })
+
+local frame = CreateFrame("Frame")
+frame:SetScript('OnEvent', function(self, event, ...) return self[event](self, event, ...) end)
+
+frame:RegisterEvent('PLAYER_ENTERING_WORLD')
+function frame:PLAYER_ENTERING_WORLD()
+	LoggingCombat(orcedEnable or (not forcedDisable and select(2, IsInInstance()) == "raid"))
+end
+
+frame:RegisterEvent('PLAYER_LEAVING_WORLD')
+function frame:PLAYER_LEAVING_WORLD()
+	LoggingCombat(false)
+end
 
 local LoggingCombat = LoggingCombat
 
@@ -32,4 +62,3 @@ hooksecurefunc('LoggingCombat', function(...)
 end)
 
 dataobj:Update()
-
